ba6021eb6cee2edbf4bd9f829009d716fa4bda22
nizam754 <m.nizam754@gmail.com>
2022-03-01 00:23:09 +0600

    Function_turtle

---
+++ Function_turtle.py
@@ -0,0 +1,15 @@
+import turtle
+turtle = turtle.Turtle()
+def square():
+    turtle.forward(90)
+    turtle.left(90)
+    turtle.forward(90)
+    turtle.left(90)
+    turtle.forward(90)
+    turtle.left(90)
+    turtle.forward(90)
+    turtle.left(90)
+
+square()
+turtle.forward(90)
+square()
