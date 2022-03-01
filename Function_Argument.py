9ab3d6c3ee0bed1c3d9e73f59d06a5b2ed2c5722
nizam754 <m.nizam754@gmail.com>
2022-03-01 07:17:03 +0600

    function arguments

---
+++ Function_Argument.py
@@ -0,0 +1,14 @@
+import turtle
+turtle = turtle.Turtle()
+def square(length):
+    turtle.forward(length)
+    turtle.left(90)
+    turtle.forward(length)
+    turtle.left(90)
+    turtle.forward(length)
+    turtle.left(90)
+    turtle.forward(length)
+    turtle.left(90)
+
+square(180/2)
+
